e5119024624b1bf6787886c56cfae0978a261bb8
Chaeyeon <chae0382@gmail.com>
2021-08-10 14:15:21 +0900

    Initial commit

---
+++ src/test/java/com/example/summercoding_spring/SummerCodingSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.summercoding_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SummerCodingSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
